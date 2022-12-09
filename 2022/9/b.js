90f23479ef551f78e4776785abcf73bf7fc61ef4
Sam <47493725+Vuurvos1@users.noreply.github.com>
2022-12-09 13:48:39 +0100

    fix potential 0 0 bug and slight refactor

--- 2022/9/b.js
+++ 2022/9/b.js
@@ -4,12 +4,12 @@ const inputFile = fs.readFileSync('input.txt', 'utf8');
 let input = inputFile.split('\n').slice(0, -1);
 
 let head = { x: 0, y: 0 };
-
 let points = new Array(10).fill(0).map((p) => {
   return { x: 0, y: 0 };
 });
 
 const moveSet = new Set();
+moveSet.add(`${head.x},${head.y}`);
 
 const moveTail = (points) => {
   for (let i = 0; i < points.length; i++) {
@@ -19,15 +19,8 @@ const moveTail = (points) => {
     // touching
     if (Math.abs(h.x - point.x) < 2 && Math.abs(h.y - point.y) < 2) return;
 
-    if (h.x == point.x) {
-      point.y += Math.sign(h.y - point.y);
-    } else if (h.y == point.y) {
-      point.x += Math.sign(h.x - point.x);
-    } else {
-      // diagonal
-      point.x += Math.sign(h.x - point.x);
-      point.y += Math.sign(h.y - point.y);
-    }
+    point.x += Math.sign(h.x - point.x);
+    point.y += Math.sign(h.y - point.y);
   }
 
   moveSet.add(`${points[points.length - 1].x},${points[points.length - 1].y}`);
@@ -37,33 +30,19 @@ for (const move of input) {
   let [dir, amount] = move.split(' ');
   amount = Number(amount);
 
-  if (dir === 'U') {
-    for (let i = 0; i < amount; i++) {
+  for (let i = 0; i < amount; i++) {
+    if (dir === 'U') {
       head.y++;
-      moveTail(points);
-    }
-  }
-
-  if (dir === 'D') {
-    for (let i = 0; i < amount; i++) {
+    } else if (dir === 'D') {
       head.y--;
-      moveTail(points);
-    }
-  }
-
-  if (dir === 'L') {
-    for (let i = 0; i < amount; i++) {
+    } else if (dir === 'L') {
       head.x--;
-      moveTail(points);
+    } else {
+      head.x++; // right
     }
-  }
 
-  if (dir === 'R') {
-    for (let i = 0; i < amount; i++) {
-      head.x++;
-      moveTail(points);
-    }
+    moveTail(points);
   }
 }
 
-console.log('out', moveSet.size + 1);
+console.log('out', moveSet.size);

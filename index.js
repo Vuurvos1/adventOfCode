a12de05159a2d9b5b765902b275e8d4e729beb61
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-12-14 08:32:44 +0100

    day 14 a

--- index.js
+++ index.js
@@ -1,8 +1,114 @@
 import fs from 'node:fs';
+import { posix } from 'node:path';
 
 const inputFile = fs.readFileSync('input.txt', 'utf8');
-let input = inputFile.split('\n\n').slice(0, -1);
+let input = inputFile
+  .split('\n')
+  .slice(0, -1)
+  .map((el) => el.split(' -> ').map((el) => el.split(',').map(Number)));
 
-console.log(input);
+// console.log(input);
+
+const cave = [];
+
+for (let i = 0; i < 1000; i++) {
+  cave.push(new Array(1000).fill('.'));
+}
+
+// console.log(cave);
+
+let maxY = 0;
+
+for (const coords of input) {
+  for (let k = 0; k < coords.length - 1; k++) {
+    const [fromX, fromY] = coords[k];
+    const [toX, toY] = coords[k + 1];
+
+    maxY = Math.max(toY, fromY, maxY);
+
+    const startX = Math.min(fromX, toX);
+    const endX = Math.max(fromX, toX) + 1;
+
+    const startY = Math.min(fromY, toY);
+    const endY = Math.max(fromY, toY) + 1;
+
+    // console.log(startX, endX, startY, endY);
+
+    for (let x = startX; x < endX; x++) {
+      for (let y = startY; y < endY; y++) {
+        cave[x][y] = '#';
+      }
+    }
+
+    // console.log(i, j);
+
+    // for
+  }
+
+  // drawing lines
+  // const coords = inp;
+}
+
+// let preview = [];
+// for (let i = 495; i < 504; i++) {
+//   let tmp = [];
+//   for (let j = 0; j < 10; j++) {
+//     tmp.push(cave[i][j]);
+//   }
+
+//   preview.push(tmp);
+// }
+
+// console.log(preview.map((el) => el.join('')));
+
+// simulation
+for (let i = 0; i < 100000; i++) {
+  const pos = { x: 500, y: 0 };
+  let moved = true;
+  while (true) {
+    if (cave[pos.x][pos.y + 1] == '.') {
+      pos.y++;
+    } else if (cave[pos.x - 1][pos.y + 1] == '.') {
+      pos.x--;
+      pos.y++;
+    } else if (cave[pos.x + 1][pos.y + 1] == '.') {
+      pos.x++;
+      pos.y++;
+    } else if (pos.y > maxY) {
+      moved = false;
+      console.log('to big');
+      break;
+    } else {
+      // cant move
+      // console.log('sand', pos.x, pos.y);
+
+      cave[pos.x][pos.y] = 'o';
+      break;
+    }
+
+    // console.log(pos);
+  }
+
+  if (!moved) {
+    console.log(i);
+    break;
+  }
+}
+
+// preview = [];
+// for (let i = 495; i < 504; i++) {
+//   let tmp = [];
+//   for (let j = 0; j < 10; j++) {
+//     tmp.push(cave[i][j]);
+//   }
+
+//   preview.push(tmp);
+// }
+
+// console.log(preview.map((el) => el.join('')));
+
+// console.log(input);
+
+// console.log(cave);
 
 console.log('out', 0);

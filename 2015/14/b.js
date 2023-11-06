6dbf72f90509f8855c42b4c3947ca95a1afbe229
Sam <47493725+Vuurvos1@users.noreply.github.com>
2023-11-06 16:52:32 +0100

    2015 day 14

---
+++ 2015/14/b.js
@@ -0,0 +1,43 @@
+import fs from 'node:fs';
+
+const input = fs.readFileSync('./2015/14/input.txt', 'utf8').trim().split('\n');
+
+const reindeer = input.map((deer) => {
+  // Rudolph can fly 22 km/s for 8 seconds, but then must rest for 165 seconds.
+  const values = deer.match(/\d+/g).map(Number);
+
+  return {
+    name: deer.split(' ')[0],
+    speed: values[0],
+    duration: values[1],
+    rest: values[2],
+    cooldown: values[1] + values[2],
+    distance: 0,
+    score: 0,
+  };
+});
+
+// TODO: speed up using some math magic
+for (let i = 0; i < 2503; i++) {
+  for (const deer of reindeer) {
+    if (deer.cooldown > deer.rest) {
+      deer.distance += deer.speed;
+      deer.cooldown--;
+      continue;
+    }
+
+    deer.cooldown--;
+
+    if (deer.cooldown <= 0) {
+      deer.cooldown = deer.rest + deer.duration;
+    }
+  }
+
+  const furthest = reindeer.sort((a, b) => b.distance - a.distance)[0].distance;
+
+  for (const deer of reindeer) {
+    if (deer.distance === furthest) deer.score++;
+  }
+}
+
+console.log(reindeer, reindeer.sort((a, b) => b.score - a.score)[0]);

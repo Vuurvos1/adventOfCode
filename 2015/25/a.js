dd1a9f51996ee3370383bce610e357770ebedeff
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-11-09 22:58:03 +0100

    2x speed up

--- 2015/25/a.js
+++ 2015/25/a.js
@@ -1,5 +1,7 @@
 import * as lib from 'lib';
 
+const size = 3030;
+
 // /**
 //  * @param {number} row
 //  * @param {number} column
@@ -21,8 +23,6 @@ import * as lib from 'lib';
 
 // console.log(getIndex(6, 1), getIndex(1, 6), getIndex(3, 3));
 
-const size = 3030;
-
 // const codes = new Uint32Array(size * size);
 // codes[0] = 20151125;
 // for (let i = 1; i < codes.length - 1; i++) {
@@ -30,25 +30,26 @@ const size = 3030;
 // }
 // there might be a way to directly lookup the value in the array
 
-// TODO: optimize
-const paper = lib.generate2dArray(size, size);
-paper[0][0] = 20151125;
+// TODO: further optimize?
 
-let index = 0;
+console.time('e');
 let last = 20151125;
+let output;
 for (let i = 0; i < size * size; i++) {
   for (let row = 0; row < i + 1; row++) {
-    // if (index % 1_000_000 === 0) console.log(index);
+    if (row === 3029 - 1 && i - row === 2947 - 1) {
+      output = last;
+      break;
+    }
 
-    if (row < size && i - row < size) paper[row][i - row] = last;
-    index++;
     last = (last * 252533) % 33554393;
-
-    if (paper[3029 - 1][2947 - 1]) break; // break out early or this will take ages to fill the entire grid
   }
+
+  if (output) break;
 }
+console.timeEnd('e');
 
 // Enter the code at row 2947, column 3029.
-
 // col - row
-console.log(paper[3029 - 1][2947 - 1]); // paper is 1 indexed
+console.log('out', output); // paper is 1 indexed
+// 19980801

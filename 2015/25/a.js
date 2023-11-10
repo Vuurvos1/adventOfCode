e17e122eb284f48350998728ad9815e52ed1960e
Sam <47493725+Vuurvos1@users.noreply.github.com>
2023-11-10 19:02:35 +0100

    cleanup

--- 2015/25/a.js
+++ 2015/25/a.js
@@ -1,36 +1,4 @@
-import * as lib from 'lib';
-
 const size = 3030;
-
-// /**
-//  * @param {number} row
-//  * @param {number} column
-//  */
-// function getIndex(row, column) {
-//   let index = 0;
-
-//   for (let i = 0; i < column; i++) {
-//     index += i;
-//   }
-//   index += column;
-
-//   for (let i = 0; i < row + 1; i++) {
-//     index += i;
-//   }
-
-//   return index;
-// }
-
-// console.log(getIndex(6, 1), getIndex(1, 6), getIndex(3, 3));
-
-// const codes = new Uint32Array(size * size);
-// codes[0] = 20151125;
-// for (let i = 1; i < codes.length - 1; i++) {
-//   codes[i] = (codes[i - 1] * 252533) % 33554393;
-// }
-// there might be a way to directly lookup the value in the array
-
-// TODO: further optimize?
 
 console.time('e');
 let last = 20151125;

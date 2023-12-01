6f632f1a5fa05343c5b417d4be69db1b340ed15d
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-12-01 19:35:24 +0100

    rename function

--- 2015/07/a.js
+++ 2015/07/a.js
@@ -1,84 +1,86 @@
-import * as lib from 'lib';
-import fs from 'node:fs';
+import * as lib from "lib";
+import fs from "node:fs";
 
-const input = fs.readFileSync('./2015/7/input.txt', 'utf8').trim().split('\n');
+const input = fs.readFileSync("./2015/7/input.txt", "utf8").trim().split("\n");
 
 /** @type Record<string, number> */
 const values = {};
 
 for (const line of input.sort((a, b) => a.length - b.length)) {
-  const args = line.split(' ');
+    const args = line.split(" ");
 
-  let val = new Uint16Array(1);
+    let val = new Uint16Array(1);
 
-  let v1 = [null];
-  let v2 = [null];
+    let v1 = [null];
+    let v2 = [null];
 
-  for (let i = 0; i < args.length; i++) {
-    const arg = args[i];
-    const nextArg = args[i + 1];
+    for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+        const nextArg = args[i + 1];
 
-    if (arg === '->') {
-      break;
+        if (arg === "->") {
+            break;
+        }
+
+        if (arg === "NOT") {
+            let tmp = lib.isValidIntegerString(nextArg)
+                ? Number(nextArg)
+                : values[nextArg];
+            if (typeof tmp === "number") v1 = Uint16Array([tmp]);
+
+            break;
+        }
+
+        if (i < 1) {
+            v1[0] = lib.isValidIntegerString(arg) ? Number(arg) : values[arg];
+            val[0] = v1[0];
+        } else {
+            v2[0] = lib.isValidIntegerString(arg) ? Number(arg) : values[arg];
+        }
     }
 
-    if (arg === 'NOT') {
-      let tmp = lib.isNumeric(nextArg) ? Number(nextArg) : values[nextArg];
-      if (typeof tmp === 'number') v1 = Uint16Array([tmp]);
+    v1[0] = null;
+    console.log(v1[0], v2[0]);
 
-      break;
+    for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+
+        if (arg === "->") {
+            values[args[i + 1]] = val[0];
+            break;
+        }
+
+        //
+        if (v1[0] === null || v2[0] === null) break;
+
+        if (arg === "AND") {
+            val[0] = v1[0] & v2[0];
+        }
+
+        if (arg === "OR") {
+            val[0] = v1[0] | v2[0];
+        }
+
+        if (arg === "LSHIFT") {
+            val[0] = v1[0] << v2[0];
+        }
+
+        if (arg === "RSHIFT") {
+            val[0] = v1[0] >> v2[0];
+        }
+
+        //
+        if (v1[0] === null) break;
+
+        if (arg === "NOT") {
+            val[0] = ~v1[0];
+        }
     }
 
-    if (i < 1) {
-      v1[0] = lib.isNumeric(arg) ? Number(arg) : values[arg];
-      val[0] = v1[0];
-    } else {
-      v2[0] = lib.isNumeric(arg) ? Number(arg) : values[arg];
-    }
-  }
-
-  v1[0] = null;
-  console.log(v1[0], v2[0]);
-
-  for (let i = 0; i < args.length; i++) {
-    const arg = args[i];
-
-    if (arg === '->') {
-      values[args[i + 1]] = val[0];
-      break;
-    }
-
-    //
-    if (v1[0] === null || v2[0] === null) break;
-
-    if (arg === 'AND') {
-      val[0] = v1[0] & v2[0];
-    }
-
-    if (arg === 'OR') {
-      val[0] = v1[0] | v2[0];
-    }
-
-    if (arg === 'LSHIFT') {
-      val[0] = v1[0] << v2[0];
-    }
-
-    if (arg === 'RSHIFT') {
-      val[0] = v1[0] >> v2[0];
-    }
-
-    //
-    if (v1[0] === null) break;
-
-    if (arg === 'NOT') {
-      val[0] = ~v1[0];
-    }
-  }
-
-  console.log(values);
+    console.log(values);
 }
 
-console.log(values['a']);
+console.log(values["a"]);
 
 // 123 -> x
 // 456 -> y
--- index.js
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs';
-const rawInput = fs.readFileSync('input.txt', 'utf8');
-const input = rawInput.trimEnd().split('\n');
-
-console.log('end', 0);

2cc420b20588eeb1c44c9f50aab880b03da4a912
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-12-01 19:34:52 +0100

    simplify

--- 2023/1/a.js
+++ 2023/1/a.js
@@ -5,12 +5,8 @@ import clipboard from "clipboardy";
 const input = fs.readFileSync("./2023/1/input.txt", "utf8").trim().split("\n");
 
 let output = input.reduce((acc, curr) => {
-    const numbers = curr.match(/\d+/g);
-    const first = numbers[0][0];
-
-    const tmp = numbers[numbers.length - 1];
-    const last = tmp[tmp.length - 1];
-    const num = Number(first + last);
+    const numbers = curr.match(/\d/g);
+    const num = Number(numbers[0] + numbers[numbers.length - 1]);
     return acc + num;
 }, 0);
 

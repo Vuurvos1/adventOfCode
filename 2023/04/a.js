d23bbd176ca5bd68a988f79df449fb7b1bea024b
Sam <47493725+Vuurvos1@users.noreply.github.com>
2023-12-05 17:35:10 +0100

    alternative solution

--- 2023/04/a.js
+++ 2023/04/a.js
@@ -6,6 +6,7 @@ const input = fs.readFileSync("./2023/04/input.txt", "utf8").trim().split("\n");
 
 let output = 0;
 
+console.time("t");
 for (const line of input) {
     const [_, nums] = line.split(": ");
     const [n, w] = nums.split("|").map((s) => lib.parseNumbers(s));
@@ -26,6 +27,23 @@ for (const line of input) {
 
     output += score;
 }
+console.timeEnd("t");
 
 console.info(output);
-clipboard.writeSync(String(output));
+
+output = 0;
+
+console.time("t2");
+for (const line of input) {
+    const [_, nums] = line.split(": ");
+    const [n, w] = nums.split("|").map((s) => lib.parseNumbers(s));
+
+    const intersections = w.filter((x) => n.includes(x)).length - 1;
+    const score = intersections >= 0 ? Math.pow(2, intersections) : 0;
+
+    output += score;
+}
+console.timeEnd("t2");
+
+console.info(output);
+// clipboard.writeSync(String(output));

fe67cec9586a470a38ba43bc9db92872e7e50fb4
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-12-11 08:31:29 +0100

    input parsing and distance calculating

---
+++ 2023/11/a.js
@@ -0,0 +1,74 @@
+import fs from "node:fs";
+import * as lib from "lib";
+import clipboard from "clipboardy";
+import exp from "node:constants";
+
+const input = fs
+    .readFileSync("./2023/11/input.txt", "utf8")
+    .trim()
+    .split("\n")
+    .map((line) => line.split(""));
+
+const expanded = [];
+
+// expand vertically
+for (const line of input) {
+    if (line.every((char) => char === ".")) {
+        expanded.push(line);
+    }
+    expanded.push(line);
+}
+
+// expand horizontally
+for (let x = 0; x < expanded[0].length; x++) {
+    let match = false;
+    for (let y = 0; y < expanded.length; y++) {
+        if (expanded[y][x] === "#") {
+            match = true;
+        }
+    }
+
+    if (match) continue;
+
+    // TODO: borked
+    console.log("inserting", x);
+    // // const length = expanded[x].length;
+    for (let y = 0; y < expanded[y].length; y++) {
+        expanded[y].splice(x, 0, ".");
+    }
+    x++; // to account for the inserted column
+}
+
+fs.writeFileSync(
+    "./2023/11/output.txt",
+    expanded.map((l) => l.join("")).join("\n")
+);
+
+// get galaxies
+const galaxies = [];
+for (let y = 0; y < expanded.length; y++) {
+    for (let x = 0; x < expanded[y].length; x++) {
+        if (expanded[y][x] === "#") {
+            galaxies.push({ x, y });
+        }
+    }
+}
+
+// get distances
+const distances = [];
+for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+        const galaxyA = galaxies[i];
+        const galaxyB = galaxies[j];
+
+        // const distance = lib.manhattanDistance(galaxyA, galaxyB);
+        const distance =
+            Math.abs(galaxyA.x - galaxyB.x) + Math.abs(galaxyA.y - galaxyB.y);
+
+        distances.push(distance);
+    }
+}
+
+let output = distances.reduce((acc, cur) => acc + cur, 0);
+console.info(output);
+clipboard.writeSync(String(output));
---
+++ 2023/11/b.js
@@ -0,0 +1,12 @@
+import fs from 'node:fs';
+import * as lib from 'lib';
+import clipboard from 'clipboardy';
+
+const input = fs.readFileSync('./2023/11/input.txt', 'utf8').trim().split('\n');
+
+let output = 0;
+
+
+
+console.info(output)
+clipboard.writeSync(String(output));

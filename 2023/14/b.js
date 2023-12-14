15615015d980dd7fafbd97a6340dbbcf0558bbc1
Sam <47493725+Vuurvos1@users.noreply.github.com>
2023-12-14 14:36:03 +0100

    slow working solution

--- 2023/14/b.js
+++ 2023/14/b.js
@@ -8,8 +8,6 @@ const input = fs
     .split("\n")
     .map((line) => line.split(""));
 
-console.log(lib.log2dArray(input));
-
 // [x, y] north, west, south, east
 const directions = [
     [0, -1],
@@ -18,48 +16,92 @@ const directions = [
     [1, 0],
 ];
 
-// move rocks
-// 1000000000
-// TODO: 1000000000 % 28 = 20?
-// TODO: do x interations to let everything settle in a cycle then check the cycle length and do the remaining
-for (let i = 0; i < 12; i++) {
-    const dir = directions[i % directions.length];
-    console.log(i, dir);
+const positions = new Map();
+const indexMap = new Map();
+let cycleSize = 0;
+let startOffset = 0;
 
-    while (true) {
-        let moved = false;
-        for (let y = 0; y < input.length; y++) {
-            for (let x = 0; x < input[y].length; x++) {
-                if (input[y][x] === "." || input[y][x] === "#") continue;
-
-                const up = input[y + dir[1]]?.[x + dir[0]];
-
-                if (up === ".") {
-                    input[y + dir[1]][x + dir[0]] = input[y][x];
-                    input[y][x] = ".";
-                    moved = true;
-                }
+function getBoardScore(board) {
+    let score = 0;
+    for (let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board[y].length; x++) {
+            if (board[y][x] === "O") {
+                score += board.length - y;
             }
         }
-
-        if (!moved) break;
     }
-
-    console.log(lib.log2dArray(input));
+    return score;
 }
 
-// console.log(lib.log2dArray(input));
+// move rocks
+for (let i = 0; i < 250; i++) {
+    for (let j = 0; j < 4; j++) {
+        const dir = directions[j % directions.length];
 
-let output = 0;
+        while (true) {
+            let moved = false;
+            for (let y = 0; y < input.length; y++) {
+                for (let x = 0; x < input[y].length; x++) {
+                    if (input[y][x] === "." || input[y][x] === "#") continue;
 
-// score board
-for (let y = 0; y < input.length; y++) {
-    for (let x = 0; x < input[y].length; x++) {
-        if (input[y][x] === "O") {
-            output += input.length - y;
+                    const up = input[y + dir[1]]?.[x + dir[0]];
+
+                    if (up === ".") {
+                        input[y + dir[1]][x + dir[0]] = input[y][x];
+                        input[y][x] = ".";
+                        moved = true;
+                    }
+                }
+            }
+
+            if (!moved) break;
         }
     }
+
+    const compressed = input.map((line) => line.join("")).join(",");
+
+    const pos = positions.get(compressed);
+    if (startOffset > 0 && cycleSize < 1 && pos && pos.index > startOffset) {
+        cycleSize = i - startOffset - 1;
+        break;
+    }
+
+    if (startOffset === 0 && pos) {
+        startOffset = i;
+    }
+
+    positions.set(compressed, { index: i, board: compressed });
+    indexMap.set(i, compressed);
 }
 
+// ((cycles - i) % (start - i)) + i - 1;
+// const endIndex =
+//     ((cycles - cycleSize) % (startOffset - cycleSize)) + cycleSize - 1;
+
+// i = cycleSize
+// ((cycles - i) % (start - i)) + i - 1;
+
+// start offset 118
+// cycle size 9
+
+const cycles = 1_000_000_000;
+
+const tmp = (cycles - startOffset) % cycleSize;
+
+// This is because my input perfectly fits
+const endIndex =
+    tmp === 0
+        ? startOffset - 1
+        : ((cycles - startOffset) % cycleSize) + startOffset - 1;
+
+const endPosition = indexMap
+    .get(endIndex)
+    .split(",")
+    .map((line) => line.split(""));
+
+// score board
+let output = getBoardScore(endPosition);
+
+// 91286 <
 console.info(output);
-clipboard.writeSync(String(output));
+// clipboard.writeSync(String(output));

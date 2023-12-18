2d3b10150cb84ce15d73635ac7c0ba789408c1e5
Sam <47493725+Vuurvos1@users.noreply.github.com>
2023-12-18 11:05:18 +0100

    p2

--- 2023/18/b.js
+++ 2023/18/b.js
@@ -25,7 +25,7 @@ const dirNums = {
 };
 
 for (const line of input) {
-    // match part between parentheses
+    // matches part between parentheses
     const hex = line.match(/\(([^)]+)\)/)[1].substring(1);
     const [depthHex, dirHex] = splitAtIndex(hex, 5);
     const depth = parseInt("0x" + depthHex, 16);
@@ -33,45 +33,42 @@ for (const line of input) {
     parsed.push(`${dirNums[dirHex]} ${depth}`);
 }
 
-const points = new Set();
+console.time("t");
+const points = [];
 const pos = { x: 0, y: 0 };
+let outlineSize = 0;
 
 // trench
 for (const line of parsed) {
     let [dir, amount, color] = line.split(" ");
     amount = Number(amount);
 
-    for (let i = 0; i < amount; i++) {
-        points.add(`${pos.x},${pos.y}`);
+    pos.x += dirs[dir].x * amount;
+    pos.y += dirs[dir].y * amount;
+    outlineSize += amount;
 
-        pos.x += dirs[dir].x;
-        pos.y += dirs[dir].y;
-    }
+    points.push([pos.x, pos.y]);
 }
 
-console.info("trench done");
+// calculate area
+let area = 0;
+for (let i = 0; i < points.length; i++) {
+    const [x1, y1] = points[i];
 
-// flood fill
-const visited = new Set();
-// This is sort of a bad way to do this, since it can flood into infinity
-const queue = [{ x: 1, y: 1 }];
-
-while (queue.length > 0) {
-    const { x, y, steps } = queue.shift();
-    const key = `${x},${y}`;
-
-    if (points.has(key)) continue;
-
-    if (visited.has(key)) continue;
-    visited.add(key);
-
-    for (const dir of lib.cardinalDirections) {
-        queue.push({ x: x + dir.x, y: y + dir.y, steps: steps + 1 });
+    let x2, y2;
+    if (i === points.length - 1) {
+        // wrap around to start
+        x2 = points[0][0];
+        y2 = points[0][1];
+    } else {
+        x2 = points[i + 1][0];
+        y2 = points[i + 1][1];
     }
+
+    area += x1 * y2 - x2 * y1;
 }
+console.timeEnd("t");
 
-let output = points.size + visited.size;
-
-console.log(points.size, points.has("0,0"));
+let output = Math.abs(area) / 2 + outlineSize / 2 + 1;
 console.info(output);
 clipboard.writeSync(String(output));

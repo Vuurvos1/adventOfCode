bf96b213344db2d3f08914874639a98eb600a413
Sam <47493725+Vuurvos1@users.noreply.github.com>
2024-12-02 11:16:15 +0100

    d2 cleanup

--- 2024/d02/src/main.rs
+++ 2024/d02/src/main.rs
@@ -4,6 +4,32 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn isSafeReport(v: Vec<i32>) -> bool {
+    let mut split = v.iter();
+    let mut sign_dir = 0;
+
+    let mut prev = split.next().unwrap();
+    for item in split {
+        let diff = (item - prev).abs();
+        if diff > 3 || diff < 1 {
+            // println!("Items are not in safe range");
+            return false;
+        }
+
+        let sign = (item - prev).signum();
+        if sign_dir == 0 {
+            sign_dir = sign;
+        } else if sign_dir != sign {
+            // println!("Items are not ascending or descending");
+            return false;
+        }
+
+        prev = item;
+    }
+
+    true
+}
+
 #[test]
 fn p1() {
     let contents =
@@ -11,103 +37,19 @@ fn p1() {
 
     let mut sum = 0;
 
-    'outer: for line in contents.lines() {
-        let mut split = line.split_whitespace().map(|x| x.parse::<i32>().unwrap());
+    for line in contents.lines() {
+        let split: Vec<i32> = line
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+        let safe = isSafeReport(split);
 
-        let mut asc = false;
-        let mut desc = false;
-
-        let mut s1 = split.clone();
-        // check if items are ascending
-        let mut prev = s1.next().unwrap();
-        for item in s1 {
-            if item < prev {
-                // println!("Items are not ascending");
-                asc = true;
-                break;
-            }
-            prev = item;
+        if safe {
+            sum += 1;
         }
-
-        let mut s2 = split.clone();
-        prev = s2.next().unwrap();
-        for item in s2 {
-            if item > prev {
-                // println!("Items are not descending");
-                desc = true;
-                break;
-            }
-            prev = item;
-        }
-
-        if !(asc || desc) || asc && desc {
-            continue;
-        }
-
-        prev = split.next().unwrap();
-        for item in split {
-            let diff = (item - prev).abs();
-            if diff > 3 || diff < 1 {
-                // println!("Items are not in safe range");
-                continue 'outer;
-            }
-            prev = item;
-        }
-
-        // println!("Line: {}", line);
-
-        sum += 1;
     }
 
     println!("Hello, world! {}", sum);
-}
-
-fn isSafeReport(v: Vec<i32>) -> bool {
-    let mut split = v.iter();
-
-    let mut asc = false;
-    let mut desc = false;
-
-    let mut s1 = split.clone();
-    // check if items are ascending
-    let mut prev = s1.next().unwrap();
-    for item in s1 {
-        if item < prev {
-            // println!("Items are not ascending");
-            asc = true;
-            break;
-        }
-        prev = item;
-    }
-
-    let mut s2 = split.clone();
-    prev = s2.next().unwrap();
-    for item in s2 {
-        if item > prev {
-            // println!("Items are not descending");
-            desc = true;
-            break;
-        }
-        prev = item;
-    }
-
-    if !(asc || desc) || asc && desc {
-        return false;
-    }
-
-    prev = split.next().unwrap();
-    for item in split {
-        let diff = (item - prev).abs();
-        if diff > 3 || diff < 1 {
-            // println!("Items are not in safe range");
-            return false;
-        }
-        prev = item;
-    }
-
-    // println!("Line: {}", line);
-
-    return true;
 }
 
 #[test]
@@ -121,7 +63,6 @@ fn p2() {
         let split = line.split_whitespace().map(|x| x.parse::<i32>().unwrap());
         let v: Vec<i32> = split.collect();
         let safe = isSafeReport(v.clone());
-
         if safe {
             sum += 1;
             continue;

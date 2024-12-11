244d2d1538d3b1e103b38853c446c6570ca583cb
Sam <47493725+Vuurvos1@users.noreply.github.com>
2024-12-11 14:32:43 +0100

    speed up

--- 2024/d11/src/main.rs
+++ 2024/d11/src/main.rs
@@ -14,68 +14,53 @@ fn main() {
     println!("p2: {:.2?}", elapsed);
 }
 
+fn step_stone(digit: u64, count: u64, step: u16, cache: &mut HashMap<(u64, u16), u64>) -> u64 {
+    if step == 0 {
+        return count + 1;
+    }
+
+    if digit == 0 {
+        // not caching the 0 - 1 seems to be faster
+        return step_stone(1, count, step - 1, cache);
+    }
+
+    if let Some(c) = cache.get(&(digit, step)) {
+        return *c;
+    }
+
+    let len = digit.ilog10() + 1;
+    if len % 2 == 0 {
+        // push halfs to new stones
+        let divisor = 10_u64.pow(len / 2);
+        let left = digit / divisor;
+        let right = digit % divisor;
+
+        let c =
+            step_stone(left, count, step - 1, cache) + step_stone(right, count, step - 1, cache);
+        cache.insert((digit, step), c);
+        return c;
+    }
+
+    let c = step_stone(digit * 2024, count, step - 1, cache);
+    cache.insert((digit, step), c);
+    return c;
+}
+
 fn p1() {
-    let mut stones: Vec<u64> = fs::read_to_string("./src/input.txt")
+    let stones: Vec<u64> = fs::read_to_string("./src/input.txt")
         .expect("Should have been able to read the file")
         .trim_end()
         .split_whitespace()
         .map(|s| s.parse::<u64>().unwrap())
         .collect();
 
-    for _ in 0..25 {
-        let mut new_stones: Vec<u64> = Vec::new();
-        for s in stones {
-            if s == 0 {
-                new_stones.push(1);
-                continue;
-            }
+    let mut cache: HashMap<(u64, u16), u64> = HashMap::new();
+    let s: u64 = stones
+        .iter()
+        .map(|stone| step_stone(*stone, 0, 25, &mut cache))
+        .sum();
 
-            let str = s.to_string();
-            if str.len() % 2 == 0 {
-                // push halfs to new stones
-                let (first, last) = str.split_at(str.len() / 2);
-                new_stones.push(first.parse::<u64>().unwrap());
-                new_stones.push(last.parse::<u64>().unwrap());
-                continue;
-            }
-
-            new_stones.push(s * 2024);
-        }
-        stones = new_stones;
-    }
-
-    println!("Hello, world! {}", stones.len());
-}
-
-fn step_stone(digit: u64, count: u64, step: u32, cache: &mut HashMap<String, u64>) -> u64 {
-    if step == 0 {
-        return count + 1;
-    }
-
-    let key = format!("{}-{}", digit, step);
-    if cache.contains_key(&key) {
-        return *cache.get(&key).unwrap();
-    }
-
-    if digit == 0 {
-        let c = step_stone(1, count, step - 1, cache);
-        cache.insert(key, c);
-        return c;
-    }
-
-    let str = digit.to_string();
-    if str.len() % 2 == 0 {
-        // push halfs to new stones
-        let (first, last) = str.split_at(str.len() / 2);
-        let c = step_stone(first.parse::<u64>().unwrap(), count, step - 1, cache)
-            + step_stone(last.parse::<u64>().unwrap(), count, step - 1, cache);
-        cache.insert(key, c);
-        return c;
-    }
-
-    let c = step_stone(digit * 2024, count, step - 1, cache);
-    cache.insert(key, c);
-    return c;
+    println!("Hello, world! {}", s);
 }
 
 fn p2() {
@@ -86,7 +71,7 @@ fn p2() {
         .map(|s| s.parse::<u64>().unwrap())
         .collect();
 
-    let mut cache: HashMap<String, u64> = HashMap::new();
+    let mut cache: HashMap<(u64, u16), u64> = HashMap::new();
     let s: u64 = stones
         .iter()
         .map(|stone| step_stone(*stone, 0, 75, &mut cache))

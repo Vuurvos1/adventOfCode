f07b448e65918200bd2ad07b4c96cdccb452c7d0
Sam <47493725+Vuurvos1@users.noreply.github.com>
2024-12-18 13:48:29 +0100

    speed

--- 2024/d18/src/main.rs
+++ 2024/d18/src/main.rs
@@ -18,178 +18,134 @@ fn main() {
     println!("p2: {:.2?}", elapsed);
 }
 
-fn inbounds<T>(x: i32, y: i32, grid: &Vec<Vec<T>>) -> bool {
-    let height = grid.len();
-    let width = grid[0].len();
-    x >= 0 && x < width as i32 && y >= 0 && y < height as i32
+fn inbounds(x: i32, y: i32, size: u32) -> bool {
+    x >= 0 && x < size as i32 && y >= 0 && y < size as i32
 }
 
-// butchered version of d16
 fn p1() {
-    let re = regex::Regex::new(r"(\d+),(\d+)").unwrap();
-    let bytes: Vec<(u32, u32)> = fs::read_to_string("./src/input.txt")
+    let grid: HashSet<(u32, u32)> = fs::read_to_string("./src/input.txt")
         .expect("Should have been able to read the file")
         .trim_end()
         .lines()
+        .take(1024)
         .map(|line| {
-            let caps = re.captures(line).unwrap();
-            (
-                caps.get(1).unwrap().as_str().parse().unwrap(),
-                caps.get(2).unwrap().as_str().parse().unwrap(),
-            )
+            let bits = line
+                .split(',')
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<u32>>();
+            (bits[0], bits[1])
         })
         .collect();
 
-    // 2d vec of 70 x 70
-    let mut grid = vec![vec![' '; 71]; 71];
-
-    for i in 0..1024 {
-        grid[bytes[i].0 as usize][bytes[i].1 as usize] = '#';
-    }
-
+    let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)];
     let start_pos = (0, 0);
     let finish_pos = (70, 70);
 
-    let mut cache: HashMap<(i32, i32, i32, i32), u64> = HashMap::new();
-    let mut stack: VecDeque<((i32, i32), (i32, i32), u64)> = VecDeque::new();
+    let mut cache: HashMap<(i32, i32), u64> = HashMap::new();
+    let mut stack: VecDeque<((i32, i32), u64)> = VecDeque::new();
     let mut min: u64 = u64::MAX;
-    stack.push_back((start_pos, (0, 1), 0));
+    stack.push_back((start_pos, 0));
 
     while stack.len() > 0 {
-        let (pos, dir, score) = stack.pop_front().unwrap();
+        let (pos, score) = stack.pop_front().unwrap();
 
-        // update cache score if current score is lower
-        if cache.contains_key(&(pos.0, pos.1, dir.0, dir.1)) {
-            let old_score = cache.get(&(pos.0, pos.1, dir.0, dir.1)).unwrap();
-            if score >= *old_score {
+        if pos == finish_pos {
+            min = min.min(score);
+            break;
+        }
+
+        if let Some(&old_score) = cache.get(&pos) {
+            if score >= old_score {
                 continue;
             }
         }
-        cache.insert((pos.0, pos.1, dir.0, dir.1), score);
+        cache.insert((pos.0, pos.1), score);
 
-        if score > min {
-            continue;
+        for &(dy, dx) in &directions {
+            let new_pos = (pos.0 + dx, pos.1 + dy);
+            if inbounds(new_pos.1, new_pos.0, 71)
+                && !grid.contains(&(new_pos.0 as u32, new_pos.1 as u32))
+            {
+                stack.push_back((new_pos, score + 1));
+            }
         }
-
-        if pos.0 == finish_pos.0 && pos.1 == finish_pos.1 {
-            min = min.min(score);
-            continue;
-        }
-
-        // move forward for 1 cost
-        let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
-        if inbounds(new_pos.1, new_pos.0, &grid)
-            && grid[new_pos.0 as usize][new_pos.1 as usize] != '#'
-        {
-            stack.push_back((new_pos, dir, score + 1));
-        }
-
-        // rotate clockwise or counterclockwise for 1000 cost
-        let new_dir = (dir.1, -dir.0);
-        stack.push_back((pos, new_dir, score));
-        let new_dir = (-dir.1, dir.0);
-        stack.push_back((pos, new_dir, score));
     }
 
     println!("Hello, world! {}", min);
 }
 
-fn print_grid(grid: &Vec<Vec<char>>) {
-    for row in grid {
-        for cell in row {
-            print!("{}", cell);
-        }
-        println!();
-    }
-}
-
-// even more butchered version of d16
 fn p2() {
-    let re = regex::Regex::new(r"(\d+),(\d+)").unwrap();
-    let bytes: Vec<(u32, u32)> = fs::read_to_string("./src/input.txt")
+    let input: Vec<(u32, u32)> = fs::read_to_string("./src/input.txt")
         .expect("Should have been able to read the file")
         .trim_end()
         .lines()
         .map(|line| {
-            let caps = re.captures(line).unwrap();
-            (
-                caps.get(1).unwrap().as_str().parse().unwrap(),
-                caps.get(2).unwrap().as_str().parse().unwrap(),
-            )
+            let bits = line
+                .split(',')
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<u32>>();
+            (bits[0], bits[1])
         })
         .collect();
 
-    // 2d vec of 70 x 70
-    let mut grid = vec![vec![' '; 71]; 71];
-
-    for i in 0..1024 {
-        grid[bytes[i].0 as usize][bytes[i].1 as usize] = '#';
-    }
-
+    let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)];
     let start_pos = (0, 0);
     let finish_pos = (70, 70);
 
-    let mut cache: HashMap<(i32, i32, i32, i32), u64> = HashMap::new();
-    let mut stack: VecDeque<((i32, i32), (i32, i32), u64)> = VecDeque::new();
+    let mut cache: HashSet<(i32, i32)> = HashSet::new();
+    let mut stack: VecDeque<(i32, i32)> = VecDeque::new();
 
     let mut blocked: (u32, u32) = (0, 0);
     let mut block_index = 0;
-    for i in 1024..bytes.len() {
-        grid[bytes[i].0 as usize][bytes[i].1 as usize] = '#';
+
+    let mut search_min = 1024;
+    let mut search_max = input.len() as i32;
+
+    loop {
         let mut found = false;
-        let mut min = u64::MAX;
         cache.clear();
         stack.clear();
-        stack.push_back((start_pos, (0, 1), 0));
+        stack.push_back(start_pos);
 
-        if i % 100 == 0 {
-            println!("{}%", i);
-        }
+        let search_mid = (search_min + search_max) / 2 + 1;
+        let map_bytes: HashSet<(u32, u32)> =
+            input.iter().take(search_mid as usize).cloned().collect();
 
         while stack.len() > 0 && !found {
-            let (pos, dir, score) = stack.pop_front().unwrap();
-
-            // update cache score if current score is lower
-            if cache.contains_key(&(pos.0, pos.1, dir.0, dir.1)) {
-                let old_score = cache.get(&(pos.0, pos.1, dir.0, dir.1)).unwrap();
-                if score >= *old_score {
-                    continue;
-                }
-            }
-            cache.insert((pos.0, pos.1, dir.0, dir.1), score);
-
-            if score > min {
-                continue;
-            }
+            let pos = stack.pop_front().unwrap();
 
             if pos.0 == finish_pos.0 && pos.1 == finish_pos.1 {
-                min = min.min(score);
                 found = true;
+                break;
+            }
+
+            if cache.contains(&pos) {
                 continue;
             }
+            cache.insert(pos);
 
-            // move forward for 1 cost
-            let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
-            if inbounds(new_pos.1, new_pos.0, &grid)
-                && grid[new_pos.0 as usize][new_pos.1 as usize] != '#'
-            {
-                stack.push_back((new_pos, dir, score + 1));
+            for &(dy, dx) in &directions {
+                let new_pos = (pos.0 + dx, pos.1 + dy);
+                if inbounds(new_pos.1, new_pos.0, 71)
+                    && !map_bytes.contains(&(new_pos.0 as u32, new_pos.1 as u32))
+                {
+                    stack.push_back(new_pos);
+                }
             }
-
-            // rotate clockwise or counterclockwise for 1000 cost
-            let new_dir = (dir.1, -dir.0);
-            stack.push_back((pos, new_dir, score));
-            let new_dir = (-dir.1, dir.0);
-            stack.push_back((pos, new_dir, score));
         }
-        if !found {
-            blocked = bytes[i];
-            block_index = i;
+
+        if found {
+            search_min = search_mid;
+        } else {
+            search_max = search_mid;
+        }
+
+        if search_max - search_min <= 1 {
+            blocked = input[search_min as usize];
+            block_index = search_min;
             break;
         }
     }
 
-    print_grid(&grid);
-
-    println!("Hello, world! {:?} {}", blocked, block_index);
+    println!("Hello, world! {},{} {}", blocked.0, blocked.1, block_index);
 }
